8c26c5e3eef250b7a1149a77f816a1be45605bb9
rajsiv12 <orcsy@s172-20-10-h121.paws.uga.edu>
2017-05-03 10:30:20 -0400

    just one N

--- average_wins.py
+++ average_wins.py
@@ -11,8 +11,8 @@ a4 = blackjack.DealerAgent(n=18)
 agents = [a1, a2, pomcp_agent, a3, a4]
 dealer = blackjack.DealerAgent()
 
-N = [10,100,1000,10000]
-n_decks = [6,7,8,9,10]
+N = 10000
+n_decks = [5,6,7,8,9,10]
 columns = ['N','Decks','POMCP','Random','Agent16','Agent17','Agent18']
 trial_num=[]
 deck_num=[]
@@ -24,22 +24,20 @@ a18_win =[]
 
 
 
-for trials in N:
-    for deck in n_decks:
-        game = blackjack.Simulator(*agents,dealer=dealer,n_decks=deck)
-        outcomes = game.run(trials)
 
-        trial_num.append(trials)
-        deck_num.append(deck)
-        dealer_scores = outcomes[dealer]
-        pomcp_win.append(sum(outcomes[pomcp_agent] > dealer_scores)/trials)
-        rand_win.append(sum(outcomes[a1] > dealer_scores)/trials)
-        a16_win.append(sum(outcomes[a2] > dealer_scores)/trials)
-        a17_win.append(sum(outcomes[a3] > dealer_scores)/trials)
-        a18_win.append(sum(outcomes[a4] > dealer_scores)/trials)
+for deck in n_decks:
+    game = blackjack.Simulator(*agents,dealer=dealer,n_decks=deck)
+    outcomes = game.run(N)
 
-scores = pd.DataFrame({'N':trial_num,
-                       'Decks': deck_num,
+    deck_num.append(deck)
+    dealer_scores = outcomes[dealer]
+    pomcp_win.append(sum(outcomes[pomcp_agent] > dealer_scores)/N)
+    rand_win.append(sum(outcomes[a1] > dealer_scores)/N)
+    a16_win.append(sum(outcomes[a2] > dealer_scores)/N)
+    a17_win.append(sum(outcomes[a3] > dealer_scores)/N)
+    a18_win.append(sum(outcomes[a4] > dealer_scores)/N)
+
+scores = pd.DataFrame({'Decks': deck_num,
                        'POMCP':pomcp_win,
                        'Random':rand_win,
                        'Agent16':a16_win,
